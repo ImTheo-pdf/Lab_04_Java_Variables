8a679816b5b9e48057df03a790aec222a0a9dbe5
ImTheo-pdf <trejacks223@gmail.com>
2025-06-09 21:47:24 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,51 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Hello and welcome!");
+        int intOperandA = 6;
+        int intOperandB = 3;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 12.67;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+
+        double myLunchCost = 12.50;
+        int numberOfKidsInFamily = 2;
+        boolean isRaining = false;
+        double gallonGasPrice = 12.90;
+        int favoriteNumber = 32;
+        double shoeSize = 9.5;
+        String birthMonth = "July";
+        String fullName = "Tre Jackson";
+
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+
+
+        System.out.println("The sum of ints of " + intOperandA + " and " + intOperandB + " is " + intSum + ".");
+        System.out.println("The product of ints of " + intOperandA + " and " + intOperandB + " is " + intProduct + ".");
+        System.out.println("The difference of ints of " + intOperandA + " and " + intOperandB + " is " + intDifference + ".");
+        System.out.println("The quotient of ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient + ".");
+        System.out.println("The modulo/remainder of ints of " + intOperandA + " and " + intOperandB + " is " + intModulo + ".");
+        System.out.println();
+        System.out.println("The sum of doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum + ".");
+        System.out.println("The product of doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct + ".");
+        System.out.println("The difference of doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference + ".");
+        System.out.println("The quotient of doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient + ".");
     }
 }
